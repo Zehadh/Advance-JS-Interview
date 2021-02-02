3d2b3930a30c16dc654a828f8ce6f655d2f874cc
Zehadh <zehadh45@gmail.com>
2021-02-02 20:02:56 +0600

    split

---
+++ split.js
@@ -0,0 +1,10 @@
+const array=[1,2,3,4,5,6,7,8,9];
+const result=array.slice(2,4);
+console.log(result);
+
+const result1=array.splice(2,5);
+console.log(result1);
+console.log(array);
+
+const together = array.join(",")
+console.log(together);
