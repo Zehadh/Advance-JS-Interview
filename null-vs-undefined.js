657fc046fb0a96ba08df67401570b801a3c754f7
Zehadh <zehadh45@gmail.com>
2021-02-02 18:48:31 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,22 @@
+//value set na korle
+// let pakhi;
+// console.log(pakhi);
+
+// return na korle
+// function add(num1,num2){
+//     const sum=num1 + num2;
+// }
+// const result=add(12,13);
+// console.log(result);
+
+//parameter pass na korle
+// function add(num1,num2){
+//     console.log(num2);
+// }
+// const result=add(12);
+
+// const person={name:"mojahid",address:"hillview"};
+// console.log(person.age);
+
+let fun=undefined;
+console.log(fun);
