276fbae2b639150a531f627db647d1eb6360576b
Zehadh <zehadh45@gmail.com>
2021-02-02 18:30:24 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+//falsy
+    //false
+    //0
+    //""
+    //NaN
+    //null
+    //undefined
+//truthy
+    //'0',' ',[]
+let a=0;
+if(a){
+    console.log("It's true.");
+}
+else{
+    console.log("It's false.");
+}
