21937ad67ab47c8182302dcb012228af4e5c5176
Zehadh <zehadh45@gmail.com>
2021-02-02 18:56:18 +0600

    (==) vs (===)

---
+++ double-vs-thriple-equal.js
@@ -0,0 +1,17 @@
+// const a=2;
+// const b="2";
+// if(a==b){
+//     console.log("It's true");
+// }
+// else{
+//     console.log("It's false");
+// }
+
+const a=2;
+const b="2";
+if(a===b){
+    console.log("It's true");
+}
+else{
+    console.log("It's false");
+}
